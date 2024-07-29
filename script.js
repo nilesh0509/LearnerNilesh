8872bdd0493b76fd14f61859eb9b70e958f68f38
NILESH <125892730+nilesh0509@users.noreply.github.com>
2024-07-29 19:39:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,10 +77,13 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbwZjVsnll7823F9AVGaW3
 
 const form = document.forms['contact-form']
 
+
+
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
-    .catch(error => console.error('Error!', error.message))
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
 })
 
