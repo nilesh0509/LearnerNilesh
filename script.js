da302249d5b2803de7c3290f003175fbfa4392bc
NILESH <125892730+nilesh0509@users.noreply.github.com>
2024-07-29 19:29:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -79,6 +79,17 @@ const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+    .catch(error => console.error('Error!', error.message))
+})
+
+const form = document.forms['contact-form']
+
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
   .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..." ))
   .then(() => { window.location.reload(); })
